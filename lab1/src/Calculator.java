da4cfbb1c2f4dda9355f76df61ab5b8c70e13a4c
TodirasClaudiu <todiras64@gmail.com>
2025-02-18 15:17:19 +0200

    Add files via upload

--- lab1/src/Calculator.java
+++ lab1/src/Calculator.java
@@ -1,7 +1,9 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
 
 public class Calculator extends JFrame {
     JButton digits[] = {
