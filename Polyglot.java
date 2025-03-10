2e5c3ed61aeef1e4086ef59f702c4e083e6d21fa
TodirasClaudiu <todiras64@gmail.com>
2025-03-10 17:22:47 +0200

    Polyglot.java

--- Polyglot.java
+++ Polyglot.java
@@ -1,9 +1,6 @@
 //import libraria principala polyglot din graalvm
 import org.graalvm.polyglot.*;
-
-
-
-
+import java.util.*;
 //clasa principala - aplicatie JAVA
 class Polyglot {
     //metoda privata pentru conversie low-case -> up-case folosind functia toupper() din R
@@ -21,20 +18,14 @@ class Polyglot {
         return resultString;
     }
 
-    private static void afisare(Value v){
-        for(int i=0;i<v.getArraySize();i++) {
-            System.out.println(v.getArrayElement(i));
-            System.out.println('\n');
-        }
-    }
-
     //metoda privata pentru evaluarea unei sume de control simple a literelor unui text ASCII, folosind PYTHON
     private static int SumCRC(String token){
+        String tokenWithoutFirstAndLast=token.substring(1,token.length()-1);
         //construim un context care ne permite sa folosim elemente din PYTHON
         Context polyglot = Context.newBuilder().allowAllAccess(true).build();
         //folosim o variabila generica care va captura rezultatul excutiei functiei PYTHON, sum()
         //avem voie sa inlocuim anumite elemente din scriptul pe care il construim spre evaluare, aici token provine din JAVA, dar va fi interpretat de PYTHON
-        Value result = polyglot.eval("python", "sum(ord(ch) for ch in '" + token + "')");
+        Value result = polyglot.eval("python","sum((5 * i**5 + 4 * i**4 + 3 * i**3 + 2* i**2 + 1*i +7) * ord(ch)" + "for i, ch in enumerate('"+tokenWithoutFirstAndLast+"'))");
         //utilizam metoda asInt() din variabila incarcata cu output-ul executiei, pentru a mapa valoarea generica la un Int
         int resultInt = result.asInt();
         // inchidem contextul Polyglot
@@ -43,20 +34,12 @@ class Polyglot {
         return resultInt;
     }
 
-    private static Value NumRND(){
-        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
-        polyglot.eval("python", "import random\n" + "v=[random.randint(1,100) for i in range(20)]");
-        Value result= polyglot.eval("python", "v");
-
-        return result;
-    }
-
     //functia MAIN
     public static void main(String[] args) {
         //construim un context pentru evaluare elemente JS
         Context polyglot = Context.create();
         //construim un array de string-uri, folosind cuvinte din pagina web:  https://chrisseaton.com/truffleruby/tenthings/
-        Value array = polyglot.eval("js", "[\"If\",\"we\",\"run\"]");
+        Value array = polyglot.eval("js", "[\"If\",\"we\",\"run\",\"the\",\"java\",\"jhi\",\"tun\",\"the\",\"avaa\"]");
         //pentru fiecare cuvant, convertim la upcase folosind R si calculam suma de control folosind PYTHON
         List<String> cuvinte =new ArrayList<>();
         List<Integer> sume_de_cntrl= new ArrayList<>();
@@ -86,4 +69,3 @@ class Polyglot {
         polyglot.close();
     }
 }
-
