247218442c88d1fdad8cdcf257046a9f9a86d0f9
TodirasClaudiu <todiras64@gmail.com>
2025-03-23 12:51:35 +0200

    lab3 non zip

---
+++ lab3/p1/src/main/kotlin/ro/mike/tuiasi/Hello.kt
@@ -0,0 +1,61 @@
+package ro.mike.tuiasi
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import java.io.BufferedWriter
+import java.io.FileWriter
+import java.io.IOException
+
+data class RSSItem(val title: String, val link: String) {
+    override fun toString(): String {
+        return "Titlu: $title\nLink: $link\n\n"
+    }
+}
+data class RSSFeed(val title: String, val link: String, val description: String,val pubDate: String, val items: MutableList<RSSItem> = mutableListOf()) {
+
+    fun addItem(item: RSSItem) {
+        items.add(item)
+    }
+
+    fun writeToFile(filename: String) {
+        try {
+            BufferedWriter(FileWriter(filename)).use { w ->
+                w.write("Flux RSS: $title\n")
+                w.write("Link-ul: $link\n")
+                w.write("Data publicarii: $pubDate\n")
+                w.write("Descriere: $description\n\n")
+                items.forEach { w.write(it.toString()) }
+            }
+        } catch (e: IOException) {
+            println("Eroare : ${e.message}")
+        }
+    }
+}
+
+fun main() {
+    val url = "https://www.antena3.ro/rss"
+    try {
+        val doc: Document = Jsoup.connect(url).get()
+        val channel: Element = doc.selectFirst("channel")!!
+
+        val titlu = channel.selectFirst("title")!!.text()
+        val link = channel.selectFirst("link")!!.text()
+        val descriere = channel.selectFirst("description")!!.text()
+        val dataPub = channel.selectFirst("pubDate")!!.text()
+        val feed = RSSFeed(titlu, link, descriere, dataPub)
+
+        val items: Elements = channel.select("item")
+        for (item in items) {
+            val itemTitlu = item.selectFirst("title")!!.text()
+            val itemLink = item.selectFirst("link")!!.text()
+            val rssItem = RSSItem(itemTitlu, itemLink)
+            feed.addItem(rssItem)
+        }
+
+        feed.writeToFile("Continut.txt")
+
+    } catch (e: IOException) {
+        println("Eroare : ${e.message}")
+    }
+}
